f893ab1d3e420c6ad1c1c41bfae601a21c2170fb
Parth-11 <parth12.mgg@gmail.com>
2025-05-10 13:39:27 +0530

    Added navBar

---
+++ lib/pages/navBar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:athelia/pages/home/homepage.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int _index = 0;
+  void _pageNavigation(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  static const List<Widget> _pages = [Homepage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: _pages.elementAt(_index),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [BottomNavigationBarItem(icon: const Icon(Icons.home))],
+          onTap: _pageNavigation,
+        ),
+      ),
+    );
+  }
+}
