5b18c140d9bb233dd93947d4eb79f19864d6025c
Parth-11 <parth12.mgg@gmail.com>
2025-05-12 12:47:52 +0530

    Added custom card and graph for stats in profile

--- lib/screens/profile/stats.dart
+++ lib/screens/profile/stats.dart
@@ -1,10 +1,75 @@
+import 'package:athelia/widgets/profile/custom_card.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class ChartData {
+  ChartData(this.x, this.y1, this.y2, this.y3, this.y4);
+  final String x;
+  final double y1;
+  final double y2;
+  final double y3;
+  final double y4;
+}
 
 class StatsSubSection extends StatelessWidget {
-  const StatsSubSection({super.key});
+  StatsSubSection({super.key});
+
+  // final List userHobbyData = [];
+  final List<ChartData> chartData = [
+    ChartData('China', 12, 10, 14, 20),
+    ChartData('USA', 14, 11, 18, 23),
+    ChartData('UK', 16, 10, 15, 20),
+    ChartData('Brazil', 18, 16, 18, 24),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CustomCard(
+              boxColor: Colors.grey.shade100,
+              heading: 'Some Text',
+              icon: Icons.bloodtype,
+              metric: 'Some Metric',
+            ),
+            CustomCard(
+              boxColor: Colors.grey.shade100,
+              heading: 'Some Text',
+              icon: Icons.bloodtype,
+              metric: 'Some Metric',
+            ),
+          ],
+        ),
+        SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          series: [
+            StackedColumnSeries<ChartData, String>(
+              dataSource: chartData,
+              xValueMapper: (data, _) => data.x,
+              yValueMapper: (data, _) => data.y1,
+            ),
+            StackedColumnSeries<ChartData, String>(
+              dataSource: chartData,
+              xValueMapper: (data, _) => data.x,
+              yValueMapper: (data, _) => data.y2,
+            ),
+            StackedColumnSeries<ChartData, String>(
+              dataSource: chartData,
+              xValueMapper: (data, _) => data.x,
+              yValueMapper: (data, _) => data.y3,
+            ),
+            StackedColumnSeries<ChartData, String>(
+              dataSource: chartData,
+              xValueMapper: (data, _) => data.x,
+              yValueMapper: (data, _) => data.y4,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
