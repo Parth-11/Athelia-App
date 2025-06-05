4ecc2ed8870b4623ff0590914b18168c4c70e5f5
Parth-11 <parth12.mgg@gmail.com>
2025-06-05 12:16:38 +0530

    Fixed the overflow of community page

--- lib/screens/community/community_page.dart
+++ lib/screens/community/community_page.dart
@@ -51,88 +51,96 @@ class _CommunityPageState extends State<CommunityPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.only(top: 14),
-          color: const Color(0xFFFFE7D1),
-          height: 70,
-          width: double.infinity,
-          child: const Text(
-            "My Cozy Corner",
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 14),
+            color: const Color(0xFFFFE7D1),
+            height: 70,
+            width: double.infinity,
+            child: const Text(
+              "My Cozy Corner",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF7A4B2A),
+              ),
+            ),
+          ),
+
+          Divider(color: Colors.brown[500], thickness: 3),
+          const Text(
+            "Cozy Lounge: Hobby Rooms",
             style: TextStyle(
-              fontSize: 28,
+              fontSize: 18,
               fontWeight: FontWeight.bold,
               color: Color(0xFF7A4B2A),
             ),
           ),
-        ),
+          const SizedBox(height: 12),
 
-        Divider(color: Colors.brown[500], thickness: 3),
-
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              const Text(
-                "Cozy Lounge: Hobby Rooms",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF7A4B2A),
-                ),
-              ),
-              const SizedBox(height: 12),
-              ...rooms.map(
-                (room) => RoomTile(
-                  room: room,
-                  onTap: () {
-                    // room enter
-                  },
-                ),
-              ),
-              const SizedBox(height: 24),
-              const Text(
-                "Featured message",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              const FeaturedMessageCard(),
-              const SizedBox(height: 24),
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFF1CD),
-                  borderRadius: BorderRadius.circular(12),
-                ),
+          Container(
+            constraints: BoxConstraints(maxHeight: 300),
+            child: Scrollbar(
+              child: SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Today’s Cozy Prompt",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text(
-                      "If your favorite hobby were a weather, what would it be?",
-                    ),
-                    const SizedBox(height: 12),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFFDAB58F),
-                        foregroundColor: Colors.white,
-                      ),
-                      onPressed: () {},
-                      child: const Text("🌥 Respond"),
-                    ),
-                  ],
+                  children:
+                      rooms
+                          .map(
+                            (room) => RoomTile(
+                              room: room,
+                              onTap: () {
+                                // room enter
+                              },
+                            ),
+                          )
+                          .toList(),
                 ),
               ),
-            ],
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 24),
+          const Text(
+            "Featured message",
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const FeaturedMessageCard(),
+          const SizedBox(height: 24),
+          Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color(0xFFFFF1CD),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Today’s Cozy Prompt",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  "If your favorite hobby were a weather, what would it be?",
+                ),
+                const SizedBox(height: 12),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFDAB58F),
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: const Text("🌥 Respond"),
+                ),
+              ],
+            ),
+          ),
+        ],
 
-      // backgroundColor: const Color(0xFFFBE9DD),
+        // backgroundColor: const Color(0xFFFBE9DD),
+      ),
     );
   }
 }
