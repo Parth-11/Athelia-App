8c58a2b7f2fa4600a61420ef7b327d0dd053b320
Gazal <gtech5277@gmail.com>
2025-06-09 16:14:59 +0530

    profile-stats chart width and colors changed

--- lib/screens/profile/stats.dart
+++ lib/screens/profile/stats.dart
@@ -16,10 +16,11 @@ class StatsSubSection extends StatelessWidget {
 
   // final List userHobbyData = [];
   final List<ChartData> chartData = [
-    ChartData('China', 12, 10, 14, 20),
+    ChartData('China', 26, 10, 14, 20),
     ChartData('USA', 14, 11, 18, 23),
     ChartData('UK', 16, 10, 15, 20),
     ChartData('Brazil', 18, 16, 18, 24),
+    ChartData('India', 23, 17, 12, 21),
   ];
 
   @override
@@ -45,31 +46,41 @@ class StatsSubSection extends StatelessWidget {
           ],
         ),
         SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
+          primaryXAxis: CategoryAxis(majorGridLines: MajorGridLines(width: 0)),
+          primaryYAxis: NumericAxis(majorGridLines: MajorGridLines(width: 0)),
+
           series: [
             StackedColumnSeries<ChartData, String>(
-              borderRadius: BorderRadius.circular(64),
+              borderRadius: BorderRadius.circular(70),
               dataSource: chartData,
               xValueMapper: (data, _) => data.x,
               yValueMapper: (data, _) => data.y1,
+              width: 0.6,
+              color: Color(0xFF9FBF6D), // Calmness
             ),
             StackedColumnSeries<ChartData, String>(
-              borderRadius: BorderRadius.circular(64),
+              borderRadius: BorderRadius.circular(70),
               dataSource: chartData,
               xValueMapper: (data, _) => data.x,
               yValueMapper: (data, _) => data.y2,
+              width: 0.6,
+              color: Color.fromARGB(255, 224, 176, 80), // Excitement
             ),
             StackedColumnSeries<ChartData, String>(
-              borderRadius: BorderRadius.circular(64),
+              borderRadius: BorderRadius.circular(50),
               dataSource: chartData,
               xValueMapper: (data, _) => data.x,
               yValueMapper: (data, _) => data.y3,
+              width: 0.6,
+              color: Color(0xFFB4B0DF), // Sadness
             ),
             StackedColumnSeries<ChartData, String>(
-              borderRadius: BorderRadius.circular(64),
+              borderRadius: BorderRadius.circular(50),
               dataSource: chartData,
               xValueMapper: (data, _) => data.x,
               yValueMapper: (data, _) => data.y4,
+              width: 0.6,
+              color: Color(0xFFD46D5D), // Stress
             ),
           ],
         ),
